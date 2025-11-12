53e8081ace0aef51293265e8edff21f06a7b452b
Alexander <bornxlo@gmail.com>
2025-11-12 14:26:59 +0100

    write vs writeline

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("I am learning C#");
+Console.WriteLine(3 + 3);
+Console.WriteLine("Remember to save, and execute 'dotnet run'");
+Console.Write("Hello world! ");
+Console.Write("This will go on the same line.");

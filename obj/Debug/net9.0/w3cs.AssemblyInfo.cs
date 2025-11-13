24d68d7a76109a4a0f21310407d2b1d00aea29db
Alexander <bornxlo@gmail.com>
2025-11-13 13:17:31 +0100

    operators (thought I already commited this, looks like assembly)

--- obj/Debug/net9.0/w3cs.AssemblyInfo.cs
+++ obj/Debug/net9.0/w3cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("w3cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58abde8737f7f58e582d8b00ad38f5826e92a29e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15c0bab09573df50dedac719dee623d7bd4f2c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("w3cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("w3cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

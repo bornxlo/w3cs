41407a5d32a33ad53c726095c25a1b8aeedc1858
Alexander <bornxlo@gmail.com>
2025-11-25 14:53:06 +0100

    reassigning conditionals

--- Program.cs
+++ Program.cs
@@ -151,5 +151,18 @@
         {
             Console.WriteLine("Good evening.");
         }
+        time = 22;
+        if (time < 10)
+        {
+            Console.WriteLine("Good morning");
+        }
+        else if (time < 20)
+        {
+            Console.WriteLine("Good day.");
+        }
+        else
+        {
+            Console.WriteLine("Good evening.");
+        }
     }
 }
